c3b9db9195158d7ed0b026511b9810dfe2e4b67c
haojie <haojie.d.yuan@gmail.com>
2020-07-27 19:07:38 +0800

    bug fix.

--- aug_search.py
+++ aug_search.py
@@ -112,7 +112,7 @@ def augmentation(img_tensor, op_type, magnitude):
 
             def build_lut(histo, step):
                 lut = (torch.cumsum(histo, 0)) + (step//2)//step
-                lut = torch.cat([torch.zeros(1), lut[:-1]])
+                lut = torch.cat([torch.zeros(1).to(im.device), lut[:-1]])
 
                 return torch.clamp(lut, 0, 255)
 
