740fe383140681a545d0211eab79cec07ab44603
haojie <haojie.d.yuan@gmail.com>
2020-08-05 21:15:31 +0800

    nip17-r1 debug completed.

---
+++ benchmark/1stHGD/Guided-Denoise-master/nips_deploy/run_defense.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# run_defense.sh is a script which executes the defense
+#
+# Envoronment which runs attacks and defences calls it in a following way:
+#   run_defense.sh INPUT_DIR OUTPUT_FILE
+# where:
+#   INPUT_DIR - directory with input PNG images
+#   OUTPUT_FILE - file to store classification labels
+#
+
+INPUT_DIR=/home/haojieyuan/Data/ImageNet/nips2017_dev
+OUTPUT_FILE=./out_list.txt
+
+CUDA_VISIBLE_DEVICES=0 python defense.py \
+  --input_dir="${INPUT_DIR}" \
+  --output_file="${OUTPUT_FILE}"
